c2f79497d995e79dd02a15608d75f8a1bd2b7394
Scubasausagee <alekalek2000@gmail.com>
2021-11-10 17:50:05 +0100

    Added room,username,location support and did some basic styling

---
+++ public/js/chat.js
@@ -0,0 +1,117 @@
+const socket=io()
+
+//Elements
+const $messageForm=document.querySelector('#message-form')
+const $messageFormInput=$messageForm.querySelector('input')
+const $messageFormButton=$messageForm.querySelector('button')
+const $locationButton=document.querySelector('#send-location')
+const $messages=document.querySelector('#messages')
+
+//Templates
+const messageTemplate=document.querySelector('#message-template').innerHTML
+const locationTemplate=document.querySelector('#location-template').innerHTML
+const sidebarTemplate=document.querySelector('#sidebar-template').innerHTML
+
+//Options
+const {username,room}=Qs.parse(location.search,{ignoreQueryPrefix:true})
+
+const autoscroll=()=>{
+    //New messsage element
+    const $newMessage=$messages.lastElementChild
+
+    //Height of new message
+    const newMessageStyles=getComputedStyle($newMessage)
+    const newMessageMargin=parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight=$newMessage.offsetHeight +newMessageMargin
+
+    //Visble height
+    const visibleHeight=$messages.offsetHeight
+
+    //Height of messages container
+    const containerHeight=$messages.scrollHeight
+
+    //How far is scrolled
+    const scrollOfset=$messages.scrollTop + visibleHeight
+
+    if(containerHeight-newMessageHeight <= scrollOfset){
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
+socket.on('message', (res) => {
+    const html=Mustache.render(messageTemplate,{
+        username: res.username,
+        message:res.text,
+        createdAt:moment(res.createdAt).format('H:mm')
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
+    autoscroll()
+})
+
+socket.on('LocationMessage',(res)=>{
+    console.log(res)
+    const html=Mustache.render(locationTemplate,{
+        username: res.username,
+        url:res.url,
+        createdAt:moment(res.createdAt).format('H:mm')
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
+    autoscroll()
+})
+
+$messageForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled','disabled')
+
+    const textInput = e.target.elements.message.value
+
+    socket.emit("SendMessage",textInput,(error)=>{
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value=''
+        $messageFormInput.focus()
+
+        if(error){
+            return console.log(error)
+        }
+
+        console.log('Message delivered')
+    })
+})
+
+$locationButton.addEventListener('click',()=>{
+
+    $locationButton.setAttribute('disabled','disabled')
+
+    if(!navigator.geolocation){
+        $locationButton.removeAttribute('disabled')
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+
+        socket.emit('sendLocation',{longitude: position.coords.longitude, latitude: position.coords.latitude},(error)=>{
+            $locationButton.removeAttribute('disabled')
+            if(error){
+                return console.log(error)
+            }
+
+            console.log('Location shared!')
+        })
+    })
+})
+
+socket.on('roomData',({room,users})=>{
+    const html=Mustache.render(sidebarTemplate,{
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML=html
+})
+
+socket.emit('join',{username , room},(error)=>{
+    if(error){
+        alert(error)
+        location.href='/'
+    }
+})
--- src/index.js
+++ src/index.js
@@ -1,16 +1,72 @@
 const express=require('express')
+const http=require('http')
 const path=require('path')
+const socketio=require('socket.io')
+const Filter=require('bad-words')
+const {generateMessage,generateLocationMesaage}=require('./utils/messages')
+const {addUser,removeUser,getUser,getUsersInRoom}=require('./utils/users')
 
 const port=process.env.PORT
 const app=express()
+const server=http.createServer(app)
+const io=socketio(server)
 
 const PublicDirrPath = path.join(__dirname, '../public')
 app.use(express.static(PublicDirrPath))
 
-app.get('',(req,res)=>{
-    res.render('index')
+io.on('connection',(socket)=>{
+    console.log('New WebSocket connection')
+
+    socket.on('join',({username,room},callback)=>{
+        const {error,user}=addUser({id:socket.id,username,room})
+        
+        if(error){
+            return callback(error)
+        }
+
+        socket.join(user.room)
+
+        socket.emit('message', generateMessage('Admin',"Welcome!"))
+        socket.broadcast.to(user.room).emit('message',generateMessage('Admin',`${user.username} has joined`))
+        io.to(user.room).emit('roomData',{
+            room:user.room,
+            users: getUsersInRoom(user.room)
+        })
+        callback()
+    })
+
+    socket.on('SendMessage', (msg,callback) => {
+        const filter=new Filter()
+
+        if(filter.isProfane(msg)){
+            return callback('Profanity is not allowed')
+        }
+
+        const user=getUser(socket.id)
+
+        io.to(user.room).emit("message", generateMessage(user.username,msg))
+        callback()
+    })
+
+    socket.on('sendLocation',(obj,callback)=>{
+        const user=getUser(socket.id)
+        io.to(user.room).emit('LocationMessage',generateLocationMesaage(user.username,`https://google.com/maps?q=${obj.latitude},${obj.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect',()=>{
+        const user=removeUser(socket.id)
+
+        if(user){
+            io.to(user.room).emit('message', generateMessage('Admin',`${user.username} has left`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+    })
 })
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log('Server up on port '+port)
 })
